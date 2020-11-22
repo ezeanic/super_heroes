842fdf2e7cf86f2f0ea55d42a0df7e04634bfd51
ezeanic <ezeanic@uindy.edu>
2020-11-22 17:42:46 -0500

    Added a little CSS

--- src/componenets/CharacterCard.js
+++ src/componenets/CharacterCard.js
@@ -3,14 +3,14 @@ function CharacterCard({characterInfo}) {
   var items=[];
   console.log(characterInfo);
   for (const [index, value] of characterInfo.entries()) {
-    items.push(<div style={{paddingLeft: "100px"}} >
-    <p><b>{characterInfo[index].name}</b></p>
+    items.push(<div style={{backgroundColor: "grey" , padding:"10px", marginLeft: "50px"}} >
+    <p style={{fontSize: "20px", backgroundColor:"white", padding:"10px"}}><b>{characterInfo[index].name}</b></p>
      <p>{characterInfo[index].biography["full-name"]}</p>
      <img src={characterInfo[index].image.url}/>
    </div>)
   }
     return (
-      <div>
+      <div><br/><br/>
         {items}
       </div>
     );
